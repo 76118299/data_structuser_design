650744543e5e4bcac84f5592be2c8b309e967989
gd <76118299@qq.com>
2020-05-31 08:33:58 +0800

    1

--- src/main/java/datastructures/sort/insert/InsertSort.java
+++ src/main/java/datastructures/sort/insert/InsertSort.java
@@ -10,15 +10,15 @@ import java.util.Arrays;
 public class InsertSort {
     public static void main(String[] args) {
         int[] arr = new int[]{9, 6, 1, 5, 3};
-        for (int i = 1; i < arr.length; i++) {
-            for (int j= i; j>0; j--) {
-                if (arr[j] < arr[j-1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                }
-            }
-        }
+          for(int i=0;i<arr.length;i++){
+              for(int j=i;j>0;j--){
+                  if(arr[j] < arr[j-1] ){
+                      int temp = arr[j];
+                      arr[j] = arr[j-1];
+                      arr[j-1] = temp;
+                  }
+              }
+          }
         System.out.println(Arrays.toString(arr));
     }
 }
--- src/main/java/datastructures/sort/selection/SelectionSort.java
+++ src/main/java/datastructures/sort/selection/SelectionSort.java
@@ -10,21 +10,16 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{5, 4, 4, 2, 3, 8, 9, 1};
-
-        //外层循环 步长++
         for (int i = 0; i < arr.length; i++) {
-            int elementIndex = i;
-            //内层需要找到最小的元素下标
+            int minPos = i;
             for (int j = i + 1; j < arr.length; j++) {
-                //先找到最小元素的下标
-                if (arr[elementIndex] > arr[j]) {
-                    elementIndex = j;
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
                 }
             }
-            //进行元素交换 把最小的元素放到数组的第一位置 第二个小的...
-            int temp = arr[elementIndex];
-            arr[elementIndex] = arr[i];
-            arr[i] = temp;
+            int tmmep = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = tmmep;
         }
         System.out.println(Arrays.toString(arr));
 
