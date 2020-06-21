77109d3d96f8f0d249ef6c137ae833915bd0fb7e
gd <76118299@qq.com>
2020-06-21 16:06:29 +0800

    1

--- src/main/java/datastructures/sort/bubble/BubbleSort.java
+++ src/main/java/datastructures/sort/bubble/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = new int[]{9, 6, 3, 1, 5};
 //        for (int i = arr.length; i > 0; i--) {
-//            for (int j = 0; j < i - 1; j++) {
+//            for (int j = 0; j < i - 1; j++) {//内层循环一周 找到第一个大的，第二周找到第二个大的
 //                if (arr[j] > arr[j + 1]) {
 //                    int temp = arr[j];
 //                    arr[j] = arr[j + 1];
--- src/main/java/datastructures/sort/insert/InsertSort.java
+++ src/main/java/datastructures/sort/insert/InsertSort.java
@@ -23,9 +23,12 @@ public class InsertSort {
 
         for(int i=0;i<arr.length;i++){
             for (int j=i;j>0;j--){
-                int temp = arr[j];
-                      arr[j] = arr[j-1];
-                      arr[j-1] = temp;
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+
             }
         }
     }
