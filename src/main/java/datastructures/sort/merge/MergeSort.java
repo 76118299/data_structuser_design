6d84d985d5ed1d247fddb83c2eb44af98d354189
gd <76118299@qq.com>
2020-08-01 16:00:39 +0800

    1

--- src/main/java/datastructures/sort/merge/MergeSort.java
+++ src/main/java/datastructures/sort/merge/MergeSort.java
@@ -10,10 +10,11 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] arr =new int[]{1,4,7,8,3,6,9};
         int[] temp = new int[arr.length];
-        int i = 0;
-        int j = 0;
-        int k = 0;
         int mid = arr.length/2;
+        int i = 0;
+        int j = mid+1;
+        int k = 0;
+
         while (i<mid && j<arr.length){
             if(arr[i]<arr[j]){
                 temp[k] = arr[i];
